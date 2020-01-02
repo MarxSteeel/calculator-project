a482d7146da42c5c671310af46e2a652ecabf8c0
MarxSteel <kurcoalt@gmail.com>
2020-01-02 16:30:58 -0300

    Added keyboard support, css and log

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 // DEFINING FUNCTIONS
 
-function add(args) {
-	const addTotal = args.reduce((total, item) => {
-		return total + item;
-	}, 0);
-	return addTotal;
-}
-
 function subtract(args) {
 	const subTotal = args.reduce((total, item) => {
 		return total - item;
 	}, (args[0]) * 2);
   return subTotal;
-}
-    
-function multiply(args) {
-  const multTotal = args.reduce((total, item) => {
-	  return total * item;
-  }, 1);
-  return multTotal;
 }
 
 function divide(args) {
@@ -26,30 +12,25 @@ function divide(args) {
 		return total / item;
 	}, (args[0]) * (args[0]));
   if (divTotal == Infinity) {
-		return "ERROR";
+		return alert("Can't divide by ZERO");
 	} else {
 		return divTotal;
 	}
 }
 
-function operate(op, args) {
-	if (op == "+") {
-		return add(args);
-	} else if (op == "-") {
-		return subtract(args);
-	} else if (op == "*") {
-		return multiply(args);
-	} else if (op == "/") {
-		return divide(args);
-	}
-}
-
-function writing(button) {
+function write(button) {
 	const display = document.querySelector("#display");
 	let val = document.querySelector(`#${button}`);
 	val.addEventListener("click", function() {
-		display.value += val.textContent;
-	})
+		if (val.textContent == "." && display.value.includes(".")) {
+			display.value = display.value;
+		} else {
+			display.value += val.textContent;
+		}
+	});
+	window.addEventListener("keydown", function(e) {
+		const key = document.querySelector(`#${button}[data-key='${e.keyCode}'`).click();
+	});
 }
 
 function parse(string) {
@@ -57,13 +38,19 @@ function parse(string) {
 	if (!isNaN(string)) {
 		return Number(string);
 	} else {
+		if (string.includes("(") && string.includes(")")) {
+			let inside = string.substring(string.indexOf("(") + 1, string.indexOf(")"));
+			let result = string.substring(0, string.indexOf("(")) + 
+										parse(inside) + string.substring(string.indexOf(")") + 1, string.length);
+			return parse(result);
+		}
 		if (string.includes("+")) {
 			let sumArray = string.split(/\+(.+)/); // Split only at first occurrence
 			return parse(sumArray[0]) + parse(sumArray[1]);
 		} else if (string.includes("-")) {
 			if (!string.match(/[+*/]/)) {
 				let subArray = string.split("-");
-				return operate("-", subArray);
+				return subtract(subArray);
 			} else {
 				let subArray = string.split(/\-(.+)/);
 				return parse(subArray[0]) - parse(subArray[1]);
@@ -74,7 +61,7 @@ function parse(string) {
 		} else if (string.includes("/")) {
 			if (!string.match(/[+*-]/)) {
 				let subArray = string.split("/");
-				return operate("/", subArray);
+				return divide(subArray);
 			}
 			let divArray = string.split(/\/(.+)/);
 			return parse(divArray[0]) / parse(divArray[1]);
@@ -82,12 +69,16 @@ function parse(string) {
 	}
 }
 
+
+
 // IMPLEMENTING THE FUNCTIONS
 
-// Making the display return to zero on refresh
+// Making the display return to zero
 
+const log = document.querySelector(".log textarea")
 const display = document.querySelector("#display");
 display.value = "";
+log.value = "";
 
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", function() {
@@ -96,32 +87,79 @@ clear.addEventListener("click", function() {
 
 // Giving the buttons functionality
 
-writing("zero");
-writing("one");
-writing("two");
-writing("three");
-writing("four");
-writing("five");
-writing("six");
-writing("seven");
-writing("eight");
-writing("nine");
-writing("add");
-writing("subtract");
-writing("multiply");
-writing("divide");
+write("zero");
+write("one");
+write("two");
+write("three");
+write("four");
+write("five");
+write("six");
+write("seven");
+write("eight");
+write("nine");
+write("add");
+write("subtract");
+write("multiply");
+write("divide");
+write("dot");
+write("open-p");
+write("close-p");
+
+// Clearing the log
+
+const logClear = document.querySelector(".log button")
+logClear.addEventListener("click", function() {
+	log.value = "";
+});
+
+// Backspace 
+
+const back = document.querySelector("#back");
+back.addEventListener("click", function() {
+	display.value = display.value.substring(0, display.value.length - 1);
+});
+window.addEventListener("keydown", function(e) {
+	if (e.keyCode == 8) {
+		display.value = display.value.substring(0, display.value.length - 1);
+	}
+});
 
 // Implementing the operations
 
 const op = document.querySelector("#op");
 op.addEventListener("click", function() {
+	log.value += display.value + "\n";
 	let result = parse(display.value);
-	display.value = result;
+	if (result % 1 === 0) {
+		display.value = result;
+		log.value += result + "\n\n";
+	} else {
+		display.value = result.toFixed(3);
+		log.value += result.toFixed(3) + "\n\n";
+	}
 });
 
+window.addEventListener("keydown", function(e) {
+	if (e.keyCode == 13) {
+		log.value += display.value + "\n";
+		let result = parse(display.value);
+		if (result % 1 === 0) {
+			display.value = result;
+			log.value += result + "\n\n";
+		} else {
+			display.value = result.toFixed(3);
+			log.value += result.toFixed(3) + "\n\n";
+		}
+	}
+	
+});
+
+// Miscellaneous
+
+if (display.value == "NaN") {
+	display.value = "";
+}
+
 // log
-// Have to fix division by zero, it displays error but it also displays whathever number you were calculating before
-// Add keyboard support
-// Clean code (add, multiply, operate)
-// Add parentheses
-// Add history
+// Add chained parentheses
+// Make it pretty
